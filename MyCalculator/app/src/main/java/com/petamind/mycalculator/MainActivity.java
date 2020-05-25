7ae53c77dc13ae214d950aadee513542d12e378c
Tung Nguyen <tungsd@gmail.com>
2020-05-26 11:33:55 +1200

    Add files via upload
    Init project 
    complete GUI
    complete button events

---
+++ MyCalculator/app/src/main/java/com/petamind/mycalculator/MainActivity.java
@@ -0,0 +1,41 @@
+package com.petamind.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        //System.out.println("Button clicked on " + v.toString());
+        TextView tv = (TextView) v;
+        TextView resultTV = findViewById(R.id.resultTV);
+        String oldText = resultTV.getText().toString();
+        switch (tv.getText().toString()){
+            case "Del":
+                if (oldText.length() > 0) {
+                    String newText = oldText.substring(0, oldText.length() - 1);
+                    resultTV.setText(newText);
+                }
+                break;
+            case "=":
+                //for tomorrow
+                break;
+            default:
+                String toAppendString = tv.getText().toString();
+                String newText = oldText + toAppendString;
+                resultTV.setText(newText);
+                //other buttons
+                break;
+        }
+    }
+}
